bf9472d58f07ec841525c01acf3759516d079684
thirasakolt-creator <thirasakolt@gmail.com>
2026-09-16 08:53:15 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'repair-v1';
+const CACHE = 'repair-v2';
 const ASSETS = ['./', './index.html', './app.js', './config.js', './manifest.json'];
 
 self.addEventListener('install', (e) => {
