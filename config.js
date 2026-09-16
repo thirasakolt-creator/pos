f5b0ca5fb6f086481ae622d157210a5570ceaff7
thirasakolt-creator <thirasakolt@gmail.com>
2026-09-16 08:15:45 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://dxgqpxhureotnbtxtqyo.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4Z3FweGh1cmVvdG5idHh0cXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MzMwMzUsImV4cCI6MjEwNTAwOTAzNX0.4hvSMh6JIjJLkRSwr9Jahwsjxkfunkz9FuAWN4DrE04',
+  WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbzXLDGRtrsyjfu8lDKYhYvfqlrOVatrjr48dgSsNkbPLw0uoEmL-3ie53saN9Vggh9-/exec',
+  TABLE: 'report_repair'
+};
